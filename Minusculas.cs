16ba3231f452759d6ede4841e197155c21b7c010
Gonzalo Cendón <gonzacendon@gmail.com>
2018-05-18 11:31:49 -0300

    Add files via upload

---
+++ Minusculas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public class Minusculas : ITest
+    {
+        public string DevolverCadena(string s)
+        {
+            return s.ToLower();
+        }
+
+        public string HolaMundo()
+        {
+            return ("¡hola mundo!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            ITest minusculas = new Minusculas();
+            ITest mayusculas = new Mayusculas();
+
+            Console.WriteLine(minusculas.HolaMundo());
+            Console.WriteLine(mayusculas.HolaMundo());
+
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine(minusculas.DevolverCadena("Esto es una prueba de INTERFACES"));
+            Console.WriteLine(mayusculas.DevolverCadena("Esto es una prueba de INTERFACES"));
+
+            Console.ReadKey();
+        }
+    }
+}
